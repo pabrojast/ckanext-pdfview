d633030b4f6aef41faa676342bb09061ac53370f
CAZALAC <pablo.ignacio.rt@gmail.com>
2024-08-01 14:20:51 -0400

    self remove

--- ckanext/pdfview/plugin.py
+++ ckanext/pdfview/plugin.py
@@ -33,14 +33,14 @@ class PDFView(p.SingletonPlugin):
 
     def view_template(self, context, data_dict):
         
-        def is_valid_domain(self, url):
+        def is_valid_domain(url):
             return url.startswith('https://data.dev-wins.com') or url.startswith('https://ihp-wins.unesco.org/')
         
         resource = data_dict["resource"]
         resource_view = data_dict["resource_view"]
 
         # Verifica si la URL necesita redirigirse al Blob Storage
-        if is_valid_domain(resource.get("url")):
+        if is_valid_domain(resource["url"]):
             upload = p.toolkit.get_action('resource_uploader')(context, resource)
             resource_view['pdf_url'] = upload.get_url_from_filename(resource['id'], resource['url'])
         else:
