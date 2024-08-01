5a62b3090c0f5a584b7e4ab736565476b1966daf
CAZALAC <pablo.ignacio.rt@gmail.com>
2024-08-01 14:08:02 -0400

    cloudstorage fix

--- ckanext/pdfview/plugin.py
+++ ckanext/pdfview/plugin.py
@@ -32,6 +32,20 @@ class PDFView(p.SingletonPlugin):
         return data_dict["resource"].get("format", "").lower() == "pdf"
 
     def view_template(self, context, data_dict):
+        
+        def is_valid_domain(self, url):
+            return url.startswith('https://data.dev-wins.com') or url.startswith('https://ihp-wins.unesco.org/')
+        
+        resource = data_dict["resource"]
+        resource_view = data_dict["resource_view"]
+
+        # Verifica si la URL necesita redirigirse al Blob Storage
+        if is_valid_domain(resource.get("url")):
+            upload = p.toolkit.get_action('resource_uploader')(context, resource)
+            resource_view['pdf_url'] = upload.get_url_from_filename(resource['id'], resource['url'])
+        else:
+            resource_view['pdf_url'] = resource.get("url")
+                    
         return "pdf_view.html"
 
     def form_template(self, context, data_dict):
